a3962e6656e2d522a6665127ffbdd1cbedcd2201
OSWorks <18616786188@qq.com>
2016-05-13 09:20:57 -0400

    no commit message

--- 01.PROJECT/src/test/java/cn/osworks/aos/mylab/asset/SmartDataSourceUtil.java
+++
@@ -1,7 +0,0 @@
-package cn.osworks.aos.mylab.asset;
-
-public class SmartDataSourceUtil {
-
-	public static void main(String[] args) {
-	}
-}
--- 04.TOOL/README.md
+++
@@ -1,2 +0,0 @@
-### AOS报表设计器：JasperStudio下载
-[http://pan.baidu.com/s/1ntHzstb](http://pan.baidu.com/s/1ntHzstb) 
