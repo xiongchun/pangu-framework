fc56a7c3185f9bc1f5d5da8420084d0948fd37ce
OSWorks <18616786188@qq.com>
2017-06-15 23:47:24 +0800

    no commit message

--- 01.SRC-BACKEND/src/system/java/aos/showcase/api/CreditCardController.java
+++ 01.SRC-BACKEND/src/system/java/aos/showcase/api/CreditCardController.java
@@ -4,17 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import aos.framework.core.asset.WebCxt;
 import aos.framework.core.typewrap.Dto;
 import aos.framework.core.typewrap.Dtos;
+import aos.framework.core.typewrap.impl.HashDto;
+import aos.framework.core.utils.AOSJson;
 
 /**
  * Http后台接口范例
  * 
- * <p>以网上信用卡申请为例子
+ * <p>
+ * 以网上信用卡申请为例子
  * 
  * @author xiongchun
  *
@@ -22,11 +27,14 @@ import aos.framework.core.typewrap.Dtos;
 @Controller
 @RequestMapping(value = "api/creditCard")
 public class CreditCardController {
-	
+
 	/**
 	 * 注册接口<br>
 	 * 
-	 * <p>这种方式的前提是H5、Android、IOS的提交方式是Get提交或POST表单提交
+	 * <p>
+	 * 这种方式的前提是H5、Android、IOS的提交方式是Get提交或POST表单提交
+	 * contentType:"application/x-www-form-urlencoded"
+	 * 
 	 * @param request
 	 * @param response
 	 */
@@ -36,19 +44,38 @@ public class CreditCardController {
 		System.out.println(inDto);
 		WebCxt.write(response, "收到数据!");
 	}
-	
+
 	/**
 	 * 注册接口<br>
-	 * <p>这种方式的前提是H5 Ajax的dataType:"json",contentType:"application/json"时或Android、IOS通过Http类库直接提交JSON的情况
+	 * <p>
+	 * 这种方式的前提是H5 Ajax的dataType:"json",contentType:"application/json"时或Android、
+	 * IOS通过Http类库直接提交JSON的情况
+	 * 
 	 * @param jsonString
 	 * @param response
 	 */
 	@RequestMapping(value = "register2")
-	public void register2(@RequestBody String jsonString,HttpServletRequest request, HttpServletResponse response) {
+	public void register2(@RequestBody String jsonString, HttpServletRequest request, HttpServletResponse response) {
 		System.out.println(jsonString);
-		//可以通过下面的API将JSON反序列化到Dto
-	    //Dto inDto = AOSJson.fromJson(jsonString, HashDto.class);
+		// 可以通过下面的API将JSON反序列化到Dto
+		Dto inDto = AOSJson.fromJson(jsonString, HashDto.class);
+		inDto.println();
 		WebCxt.write(response, "收到数据!");
 	}
-	
+
+	/**
+	 * resetful风格的Http接口
+	 * 
+	 * @param version
+	 * @param id
+	 * @param request
+	 * @param response
+	 */
+	@RequestMapping(value = "getCardInfo/{version}/{id}", method = RequestMethod.GET)
+	public void getCardInfo(@PathVariable String version, @PathVariable String id, HttpServletRequest request,
+			HttpServletResponse response) {
+		System.out.println(version);
+		WebCxt.write(response, "收到数据!");
+	}
+
 }
