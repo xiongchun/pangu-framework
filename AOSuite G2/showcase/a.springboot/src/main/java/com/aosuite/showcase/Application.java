1734ecc48e0b54bae03acba37a209c46b6673239
OSWorks <18616786188@qq.com>
2017-07-08 21:49:38 +0800

    no commit message

--- AOSuite G2/showcase/a.springboot/src/main/java/com/aosuite/showcase/Application.java
+++ AOSuite G2/showcase/a.springboot/src/main/java/com/aosuite/showcase/Application.java
@@ -1,12 +1,8 @@
 package com.aosuite.showcase;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.aosuite.showcase.service.DemoService;
 
 /**
  * SpringBoot应用入口
@@ -16,19 +12,10 @@ import com.aosuite.showcase.service.DemoService;
  */
 @SpringBootApplication
 @ComponentScan(basePackages={"com.aosuite"}) 
-public class Application implements CommandLineRunner{
-
-	@Autowired
-	private DemoService demoService;
-	
-	@Override
-	public void run(String... arg0) throws Exception {
-		demoService.getSum(2);
-	}
+public class Application{
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-		//SpringApplication.run(Application.class, args).close();
 	}
 
 }
