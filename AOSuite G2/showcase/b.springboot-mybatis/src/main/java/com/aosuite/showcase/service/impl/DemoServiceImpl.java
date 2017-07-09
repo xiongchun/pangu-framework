1d833c51cd9b5eee861d195c7a72a21b844daea9
OSWorks <18616786188@qq.com>
2017-07-10 00:04:26 +0800

    no commit message

--- AOSuite G2/showcase/b.springboot-mybatis/src/main/java/com/aosuite/showcase/service/impl/DemoServiceImpl.java
+++ AOSuite G2/showcase/b.springboot-mybatis/src/main/java/com/aosuite/showcase/service/impl/DemoServiceImpl.java
@@ -1,6 +1,5 @@
 package com.aosuite.showcase.service.impl;
 
-import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
