c5740c89a302c39b9fd15af1c65872661898c6d0
OSWorks <18616786188@qq.com>
2017-07-09 21:07:58 +0800

    no commit message

--- AOSuite G2/showcase/b.springboot-mybatis/src/main/java/com/aosuite/showcase/dao/impl/DemoDaoImpl.java
+++ AOSuite G2/showcase/b.springboot-mybatis/src/main/java/com/aosuite/showcase/dao/impl/DemoDaoImpl.java
@@ -1,11 +1,16 @@
 package com.aosuite.showcase.dao.impl;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.aosuite.showcase.dao.DemoDao;
 
 @Repository
 public class DemoDaoImpl implements DemoDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
 
 	@Override
 	public double getPrice() {
