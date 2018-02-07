a1820b3d866d88153437e57385edbfd77c421096
阿老表 <18616786188@qq.com>
2018-02-07 14:25:10 +0800

    no commit message

---
+++ myclouds-admin-web/src/main/java/com/gitee/myclouds/admin/ui/modules/enums/EnumsController.java
@@ -0,0 +1,44 @@
+package com.gitee.myclouds.admin.ui.modules.enums;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gitee.myclouds.admin.ui.modules.param.ParamService;
+import com.gitee.myclouds.toolbox.wrap.Dto;
+import com.gitee.myclouds.toolbox.wrap.Dtos;
+
+/**
+ * 键值参数管理
+ * 
+ * @author xiongchun
+ *
+ */
+@Controller
+@RequestMapping("param")
+public class EnumsController {
+
+	@Autowired
+	private ParamService paramService;
+
+	@RequestMapping("init")
+	public String init(ModelMap map) {
+
+		return "modules/param";
+	}
+
+	@RequestMapping(value = "list", method = { RequestMethod.POST, RequestMethod.GET }, produces = "application/json")
+	@ResponseBody
+	public String list(HttpServletRequest request, HttpServletResponse response) {
+		Dto inDto = Dtos.newDto(request);
+		String jsonString = paramService.getParams(inDto);
+		return jsonString;
+	}
+	
+}
