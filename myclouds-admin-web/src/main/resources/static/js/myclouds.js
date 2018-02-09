9011df4fcb6df9e9002705c8a32b920aaa966e3d
阿老表 <18616786188@qq.com>
2018-02-10 00:30:38 +0800

    no commit message

--- myclouds-admin-web/src/main/resources/static/js/myclouds.js
+++ myclouds-admin-web/src/main/resources/static/js/myclouds.js
@@ -20,32 +20,113 @@ var My = function() {
 		});
 		return formParams;
 	}
-	
+
 	/**
 	 * 弹出窗口
 	 * 
 	 * @selector JQuery支持的元素选择器表达式
 	 */
-	var popup = function(selector){
-        $.magnificPopup.open({
-            items: {
-                src: selector
-            },
-            mainClass : $(selector).attr('data-effect')
-        });		
+	var popup = function(selector) {
+		$.magnificPopup.open({
+			items : {
+				src : selector
+			},
+			mainClass : $(selector).attr('data-effect')
+		});
 	}
-	
+
 	/**
 	 * 关闭弹出窗口
 	 */
-	var close = function(){
+	var close = function() {
 		$.magnificPopup.close();
+	}
+
+	/**
+	 * 消息通知
+	 */
+	var notify = function(text, type, stack) {
+		if (typeof (text) == "undefined" || text == null) {
+			text = '^_^';
+		}
+		if (typeof (type) == "undefined" || type == null) {
+			type = 'success';
+		}
+		if (typeof (stack) == "undefined" || stack == null) {
+			stack = 'stack_top_right';
+		}
+		// Create new Notification
+		new PNotify({
+			title : '提示',
+			text : text,
+			opacity : 1,
+			addclass : stack,
+			type : type,
+			stack : Stacks[stack],
+			width : '25%',
+			delay : 500
+		});
 	}
 
 	return {
 		params : params,
 		popup : popup,
-		close : close
+		close : close,
+		notify : notify
 	}
 
 }();
+
+/* ====== 消息通知配置项 ====== */
+// A "stack" controls the direction and position
+// of a notification. Here we create an array w
+// with several custom stacks that we use later
+var Stacks = {
+	stack_top_right : {
+		"dir1" : "down",
+		"dir2" : "left",
+		"push" : "top",
+		"spacing1" : 10,
+		"spacing2" : 10
+	},
+	stack_top_left : {
+		"dir1" : "down",
+		"dir2" : "right",
+		"push" : "top",
+		"spacing1" : 10,
+		"spacing2" : 10
+	},
+	stack_bottom_left : {
+		"dir1" : "right",
+		"dir2" : "up",
+		"push" : "top",
+		"spacing1" : 10,
+		"spacing2" : 10
+	},
+	stack_bottom_right : {
+		"dir1" : "left",
+		"dir2" : "up",
+		"push" : "top",
+		"spacing1" : 10,
+		"spacing2" : 10
+	},
+	stack_bar_top : {
+		"dir1" : "down",
+		"dir2" : "right",
+		"push" : "top",
+		"spacing1" : 0,
+		"spacing2" : 0
+	},
+	stack_bar_bottom : {
+		"dir1" : "up",
+		"dir2" : "right",
+		"spacing1" : 0,
+		"spacing2" : 0
+	},
+	stack_context : {
+		"dir1" : "down",
+		"dir2" : "left",
+		"push" : "top",
+		"context" : $("#stack_context")
+	},
+}
