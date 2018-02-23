9b571cd0a0e6a0fa33ac7e9089b192916d01841d
阿老表 <18616786188@qq.com>
2018-02-23 10:12:07 +0800

    no commit message

--- myclouds-admin-web/src/main/java/com/gitee/myclouds/admin/web/login/LoginController.java
+++ myclouds-admin-web/src/main/java/com/gitee/myclouds/admin/web/login/LoginController.java
@@ -1,8 +1,16 @@
 package com.gitee.myclouds.admin.web.login;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gitee.myclouds.toolbox.wrap.Dto;
+import com.gitee.myclouds.toolbox.wrap.Dtos;
 
 /**
  * 登录页
@@ -13,10 +21,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+	@RequestMapping("login")
     public String login(ModelMap map) {
 		
         return "login";  
     }
+	
+	@RequestMapping(value = "login/go", method = { RequestMethod.POST}, produces = "application/json")
+	@ResponseBody
+	public Dto deleteParam(HttpServletRequest request, HttpServletResponse response) {
+		Dto inDto = Dtos.newDto(request);
+		return Dtos.newDto();
+	}
 	
 }
