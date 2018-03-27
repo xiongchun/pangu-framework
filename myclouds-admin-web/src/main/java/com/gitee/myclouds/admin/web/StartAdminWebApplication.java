981d9cf496dc5c5b206bc69eb8cd4b4107b10e3a
阿老表 <18616786188@qq.com>
2018-03-27 21:19:20 +0800

    no commit message

--- myclouds-admin-web/src/main/java/com/gitee/myclouds/admin/web/StartAdminWebApplication.java
+++ myclouds-admin-web/src/main/java/com/gitee/myclouds/admin/web/StartAdminWebApplication.java
@@ -2,8 +2,13 @@ package com.gitee.myclouds.admin.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.gitee.myclouds.admin.web.filters.LoginFilter;
+import com.gitee.myclouds.admin.web.filters.RequestFilter;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,5 +18,21 @@ public class StartAdminWebApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(StartAdminWebApplication.class, args);
 	}
+	
+	@Bean  
+    public FilterRegistrationBean  regRequestFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();  
+        filterRegistrationBean.setFilter(new RequestFilter());
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;  
+    } 
+	
+	@Bean  
+    public FilterRegistrationBean  regAuthFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();  
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(2);
+        return filterRegistrationBean;  
+    }  
 	
 }
