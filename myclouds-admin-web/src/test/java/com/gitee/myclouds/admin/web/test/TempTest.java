cb934b08aa2aef0f296bc8fbf89d73de86d6163e
阿老表 <18616786188@qq.com>
2018-03-27 21:20:46 +0800

    no commit message

---
+++ myclouds-admin-web/src/test/java/com/gitee/myclouds/admin/web/test/TempTest.java
@@ -0,0 +1,12 @@
+package com.gitee.myclouds.admin.web.test;
+
+import com.gitee.myclouds.toolbox.util.MyCons;
+import com.gitee.myclouds.toolbox.util.MyUtil;
+
+public class TempTest {
+
+	public static void main(String[] args) {
+		System.out.println(MyUtil.password(MyCons.PWD_KEY, "96e79218965eb72c92a549dd5a330112"));
+	}
+
+}
