95f8298bfe46bf65443a111f062434c95c422805
阿老表 <18616786188@qq.com>
2018-04-13 19:58:18 +0800

    no commit message

--- myclouds-admin/src/test/java/com/gitee/myclouds/admin/test/TempTest.java
+++ myclouds-admin/src/test/java/com/gitee/myclouds/admin/test/TempTest.java
@@ -1,19 +1,9 @@
 package com.gitee.myclouds.admin.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.gitee.myclouds.admin.modules.cache.CacheCfgService;
-import com.gitee.myclouds.common.MyCacheCxt;
-
 /*@RunWith(SpringRunner.class)
 @SpringBootTest(classes=TempTest.class)
 @ComponentScan(basePackages={"com.gitee.myclouds"})*/ 
 public class TempTest {
-
-	@Autowired
-	private MyCacheCxt myCacheCxt;
-	@Autowired
-	private CacheCfgService cacheCfgService;
 	
 	/*@Test*/
 	public  void test1() {
