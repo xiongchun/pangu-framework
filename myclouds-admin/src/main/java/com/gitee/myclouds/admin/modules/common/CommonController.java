d6464b8760f8f56cbe34d95ebedcca0f63750aed
阿老表 <18616786188@qq.com>
2018-04-13 19:57:18 +0800

    no commit message

---
+++ myclouds-admin/src/main/java/com/gitee/myclouds/admin/modules/common/CommonController.java
@@ -0,0 +1,16 @@
+package com.gitee.myclouds.admin.modules.common;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 公用 服务发布
+ * 
+ * @author xiongchun
+ *
+ */
+@RestController
+@RequestMapping("common")
+public class CommonController {
+	
+}
