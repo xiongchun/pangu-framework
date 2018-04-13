e6e5d605eebccd03e511305481dd70d4652ed381
阿老表 <18616786188@qq.com>
2018-04-13 19:57:27 +0800

    no commit message

---
+++ myclouds-admin/src/main/java/com/gitee/myclouds/admin/modules/common/CommonService.java
@@ -0,0 +1,14 @@
+package com.gitee.myclouds.admin.modules.common;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 公用服务
+ * 
+ * @author xiongchun
+ *
+ */
+@Service
+public class CommonService {
+
+}
