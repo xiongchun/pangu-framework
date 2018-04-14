2d8c026ab6528abd99486c7defb8e0ef724be5c8
阿老表 <18616786188@qq.com>
2018-04-14 21:08:04 +0800

    no commit message

--- myclouds-admin/src/main/java/com/gitee/myclouds/admin/StartAdminApplication.java
+++ myclouds-admin/src/main/java/com/gitee/myclouds/admin/StartAdminApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.gitee.myclouds.admin.modules.cache.CacheService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,10 +16,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = {"com.gitee.myclouds"}, sqlSessionFactoryRef = "sqlSessionFactory")
 @ComponentScan(basePackages={"com.gitee.myclouds"})
 public class StartAdminApplication {
-
+	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(StartAdminApplication.class, args);
-		
+		ConfigurableApplicationContext  context = SpringApplication.run(StartAdminApplication.class, args);
+		CacheService cacheService = context.getBean(CacheService.class);
+		cacheService.initCacheWhenSysBoot();
 	}
 
 }
