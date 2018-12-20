e26298f65b139628f80014c6b0b7bc73a16c5aae
阿老表 <18616786188@qq.com>
2018-12-20 15:39:40 +0800

    6. [升级]将springboot版本从1.5.8升级到2.0.7版本。 7. [升级]将springcloud版本升级到了Finchley.SR2版本。 8. [优化]优化了因springboot和springcloud版本升级后的服务注册&服务发现相关的诸多问题。

--- myclouds-admin-webserver/src/main/java/com/gitee/myclouds/admin/web/MyAdminWebServer.java
+++ myclouds-admin-webserver/src/main/java/com/gitee/myclouds/admin/web/MyAdminWebServer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
