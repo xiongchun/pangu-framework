454002f761a8820f31b46a09e958d58c06e7d7a5
xiongchun <xiongchun8211@zto.com>
2020-04-18 14:53:26 +0800

    备份static文件

---
+++ myclouds-static/layuiadmin/json/content/comment.js
@@ -0,0 +1,65 @@
+{
+  "code": 0
+  ,"msg": ""
+  ,"count": "100"
+  ,"data": [{
+"id":1,
+"dic_type":"sex",
+"dic_name":"性别",
+"dic_key":"1",
+"dic_value":"男",
+"is_enable":"1",
+"type":"1",
+"sort_no":1,
+"update_time":"2018\/2\/5 11:35:51",
+"remark":null
+},
+{
+"id":2,
+"dic_type":"sex",
+"dic_name":"性别",
+"dic_key":"2",
+"dic_value":"女",
+"is_enable":"2",
+"type":"1",
+"sort_no":2,
+"update_time":"2018\/2\/5 11:36:15",
+"remark":null
+},
+{
+"id":3,
+"dic_type":"sex",
+"dic_name":"性别",
+"dic_key":"3",
+"dic_value":"未知",
+"is_enable":"1",
+"type":"1",
+"sort_no":3,
+"update_time":"2018\/2\/5 11:38:51",
+"remark":null
+},
+{
+"id":4,
+"dic_type":"is",
+"dic_name":"全局通用是否",
+"dic_key":"1",
+"dic_value":"是",
+"is_enable":"1",
+"type":"1",
+"sort_no":1,
+"update_time":"2018\/2\/5 11:39:29",
+"remark":null
+},
+{
+"id":5,
+"dic_type":"is",
+"dic_name":"全局通用是否",
+"dic_key":"0",
+"dic_value":"否",
+"is_enable":"1",
+"type":"1",
+"sort_no":2,
+"update_time":"2018\/2\/5 11:39:57",
+"remark":null
+}]
+}
