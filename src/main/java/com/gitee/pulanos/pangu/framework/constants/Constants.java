e32702ce81985aecb42d0569614932a3d9e59808
xiongchun <18616786188@qq.com>
2021-09-03 15:31:37 +0800

    reflect the Result.java

--- src/main/java/com/gitee/pulanos/pangu/framework/constants/Constants.java
+++ src/main/java/com/gitee/pulanos/pangu/framework/constants/Constants.java
@@ -2,22 +2,45 @@ package com.gitee.pulanos.pangu.framework.constants;
 
 /**
  * 平台通用常量表
- * 
+ *
  * @author xiongchun
  * @since 1.0.0
  */
 public class Constants {
-    
+
     /**
-     * Result状态码
+     * 接口请求返回状态码
      */
-    public static class Result {
-        //通用成功状态码
-        public static final String SUCCESS = "0";
-        //通用失败状态码
-        public static final String FAIL = "-1";
+    public static class Code {
+        /**
+         * 接口调用成功，调用结果请参考具体API对应的业务返回参数。
+         */
+        public static final String SUCCESS = "10000";
+        /**
+         * 服务暂不可用（网关异常错误）
+         */
+        public static final String ERROR_GATEWAY = "40000";
+        /**
+         * 服务暂不可用（业务服务异常错误）
+         */
+        public static final String ERROR_SERVICE = "40001";
+        /**
+         * 授权错误或权限不足
+         */
+        public static final String FAIL_AUTH = "40002";
+        /**
+         * 缺少必选参数
+         */
+        public static final String FAIL_MISSING_ARG = "40002";
+        /**
+         * 非法的参数
+         */
+        public static final String FAIL_INVALID_ARG = "40003";
+        /**
+         * 接口限流（调用次数或调用频率超限）
+         */
+        public static final String FAIL_CALL_LIMITED = "40003";
+
     }
-    
-    
-    
+
 }
--- src/test/java/com/gitee/pulanos/pangu/framework/Test.java
+++ src/test/java/com/gitee/pulanos/pangu/framework/Test.java
@@ -1,10 +1,12 @@
 package com.gitee.pulanos.pangu.framework;
 
+import com.gitee.pulanos.pangu.framework.constants.Constants;
 import com.gitee.pulanos.pangu.framework.model.Result;
 
 public class Test {
 
     public static void main(String[] args) {
-        Result.create().setCode("").setData("");
+        Result.make().setCode("").setData("");
+        Result.fail(Constants.Code.ERROR_SERVICE, "");
     }
 }
