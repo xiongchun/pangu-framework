236435e37569ecc92045bc85f3aa7f56160f5d46
xiongchun <18616786188@qq.com>
2021-09-19 17:00:21 +0800

    新增maven插件

--- pangu-framework-builder-maven-plugin/src/main/java/com/gitee/pulanos/pangu/framework/builder/PanGuBuilderMojo.java
+++ pangu-framework-builder-maven-plugin/src/main/java/com/gitee/pulanos/pangu/framework/builder/PanGuBuilderMojo.java
@@ -3,18 +3,23 @@ package com.gitee.pulanos.pangu.framework.builder;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * maven插件入口
  *
- * @author xc
+ * @author xiongchun
  */
 @Mojo(name = "builder")
 public class PanGuBuilderMojo extends AbstractMojo {
 
+    @Parameter(property = "driverName")
+    private String driverName;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        System.out.println(driverName);
     }
 }
