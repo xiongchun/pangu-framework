0f95d92adfd62a6d01678cdd53a1b436bc1ef3ce
xiongchun <18616786188@qq.com>
2021-09-20 15:06:10 +0800

    新增maven插件

--- pangu-framework-builder-maven-plugin/src/main/java/com/gitee/pulanos/pangu/framework/builder/PanGuBuilderMojo.java
+++ pangu-framework-builder-maven-plugin/src/main/java/com/gitee/pulanos/pangu/framework/builder/PanGuBuilderMojo.java
@@ -1,25 +1,43 @@
 package com.gitee.pulanos.pangu.framework.builder;
 
+import cn.hutool.json.JSONUtil;
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.Map;
+
 /**
- * maven插件入口
+ * 插件入口
  *
  * @author xiongchun
  */
 @Mojo(name = "builder")
 public class PanGuBuilderMojo extends AbstractMojo {
 
-    @Parameter(property = "driverName")
-    private String driverName;
+    @Parameter(property = "url")
+    private String url;
+    @Parameter(property = "user")
+    private String user;
+    @Parameter(property = "password")
+    private String password;
 
+    @SneakyThrows
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println(driverName);
+    public void execute() {
+        Connection conn = DriverManager.getConnection(url, user, password);
+        QueryRunner run = new QueryRunner();
+        List<Map<String, Object>> result = run.query(conn,
+                "SELECT * FROM user WHERE name = ?", new MapListHandler(), "XC");
+        System.out.println(JSONUtil.toJsonStr(result));
+        DbUtils.closeQuietly(conn);
     }
+
 }
