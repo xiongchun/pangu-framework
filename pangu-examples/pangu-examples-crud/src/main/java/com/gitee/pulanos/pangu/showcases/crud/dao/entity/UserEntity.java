6257426bca570de8ee9c0a918d94f0fab9dac149
xiongchun <18616786188@qq.com>
2021-12-09 22:59:45 +0800

    修改范例 将参数配置中心配置改为本地配置，方便演示和阅读代码。仅保留演示配置中心功能的范例使用配置中心来配置参数。

--- pangu-examples/pangu-examples-crud/src/main/java/com/gitee/pulanos/pangu/showcases/crud/dao/entity/UserEntity.java
+++ pangu-examples/pangu-examples-crud/src/main/java/com/gitee/pulanos/pangu/showcases/crud/dao/entity/UserEntity.java
@@ -5,13 +5,14 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 import java.io.Serializable;
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
  * 用户信息表
  * <p>此文件由代码生成器自动生成</p>
  *
  * @author 普蓝开源社区
- * @date 2021-11-15 21:54:22
+ * @date 2021-12-09 22:30:48
  */
 @Data
 @Accessors(chain = true)
@@ -21,39 +22,48 @@ public class UserEntity implements Serializable {
    private static final long serialVersionUID=1L;
 
    /**
-     * 流水号
+    * 流水号
     */
    @TableId(value = "id", type = IdType.AUTO)
    private Long id;
 
    /**
-     * 姓名
+    * 姓名
     */
    @TableField(value = "name")
    private String name;
 
    /**
-     * 年龄
+    * 生日
+    */
+   @TableField(value = "birthday")
+   @JsonFormat(pattern="yyyy-MM-dd")
+   private Date birthday;
+
+   /**
+    * 年龄
     */
    @TableField(value = "age")
    private Integer age;
 
    /**
-     * 用户类型
+    * 用户类型
     */
    @TableField(value = "user_type")
    private String userType;
 
    /**
-     * 创建时间
+    * 创建时间
     */
    @TableField(value = "gmt_create")
+   @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
    private Date gmtCreate;
 
    /**
-     * 更新时间
+    * 更新时间
     */
    @TableField(value = "gmt_update")
+   @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
    private Date gmtUpdate;
 
 }
--- pangu-examples/pangu-examples-crud/src/main/java/com/gitee/pulanos/pangu/showcases/crud/dao/mapper/UserMapper.java
+++ pangu-examples/pangu-examples-crud/src/main/java/com/gitee/pulanos/pangu/showcases/crud/dao/mapper/UserMapper.java
@@ -9,7 +9,7 @@ import com.gitee.pulanos.pangu.showcases.crud.dao.entity.UserEntity;
  * <p>此文件由代码生成器自动生成</p>
  *
  * @author 普蓝开源社区
- * @date 2021-11-15 21:54:22
+ * @date 2021-12-09 22:30:48
  */
 @Mapper
 public interface UserMapper extends BaseMapper<UserEntity> {
