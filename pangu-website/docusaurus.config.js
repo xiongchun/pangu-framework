5b6bd1a0e03d3a838907405f3b553fd62d87288b
xiongchun <18616786188@qq.com>
2021-12-28 16:38:02 +0800

    创建项目官网pang-website，编写开发指南

--- pangu-website/docusaurus.config.js
+++ pangu-website/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
 				docs: {
 					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
-					editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+					editUrl: 'https://gitee.com/pulanos/pangu-framework/tree/master/pangu-website/',
 				},
 				blog: {
 					showReadingTime: true,
