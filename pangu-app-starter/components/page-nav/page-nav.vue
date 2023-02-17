537fa2d213ad72f812df75229bb57bdb7946cb1e
xiongchun <18616786188@qq.com>
2023-02-17 11:51:02 +0800

    pangu-app-starter init commit.

---
+++ pangu-app-starter/components/page-nav/page-nav.vue
@@ -0,0 +1,102 @@
+<template>
+	<view class="nav-wrap">
+		<view class="nav-title">
+			<u--image :showLoading="true" src="/static/common/img/logo.png" width="80px"
+				height="70px" />
+			<view class="nav-info">
+				<view class="nav-info__title">
+					<text class="nav-info__title__text">PanGu APP Starter</text>
+				</view>
+				<text class="nav-slogan">盘古移动端多平台快速开发脚手架</text>
+			</view>
+		</view>
+		<text class="nav-desc">{{desc}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			desc: String,
+			title: String,
+		},
+		data() {
+			return {
+				//version: uni.$u.config.v
+			}
+		},
+		methods: {
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.nav-wrap {
+		padding-bottom: 15px;
+		position: relative;
+	}
+
+	.lang {
+		position: absolute;
+		top: 15px;
+		right: 15px;
+	}
+
+	.nav-title {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+	}
+
+	.nav-info {
+		margin-left: 15px;
+		
+		&__title {
+			/* #ifndef APP-NVUE */
+			display: flex;
+			/* #endif */
+			flex-direction: row;
+			align-items: center;
+			
+			&__text {
+				/* #ifndef APP-NVUE */
+				display: flex;
+				/* #endif */
+				color: $u-main-color;
+				font-size: 25px;
+				font-weight: bold;
+				text-align: left;
+			}
+			
+			&__jump {
+				font-size: 12px;
+				color: $u-primary;
+				font-weight: normal;
+				margin-left: 20px;
+			}
+		}
+	}
+
+	.logo {
+		width: 70px;
+		height: 70px;
+		/* #ifndef APP-NVUE */
+		height: auto;
+		/* #endif */
+	}
+
+	.nav-slogan {
+		color: $u-tips-color;
+		font-size: 14px;
+	}
+
+	.nav-desc {
+		margin-top: 10px;
+		font-size: 14px;
+		color: $u-content-color;
+		line-height: 20px;
+	}
+</style>
---
+++ pangu-app-starter/pages/example/template.config.js
@@ -0,0 +1,11 @@
+export default [{
+		groupName: '模板',
+		groupName_en: 'Basic components',
+		list: [{
+			path: '/pages/componentsB/color/color',
+			icon: 'color',
+			title: 'Color 色彩',
+			title_en: 'Color'
+		}]
+	}
+]
